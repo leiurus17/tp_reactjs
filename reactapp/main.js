2d98af5c814e2f33d712195c5b8567d0625811cd
Marlon <martianworm17@gmail.com>
2017-11-05 16:08:08 +0800

    Fixed wrong getElementById

--- reactapp/main.js
+++ reactapp/main.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementByID('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
